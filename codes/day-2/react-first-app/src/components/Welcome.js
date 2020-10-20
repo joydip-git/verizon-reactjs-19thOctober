67e9c28f4d8274eb54fff64cbc968edf767325b8
siemens-c2c <joy_mon78@yahoo.co.in>
2020-10-20 18:29:01 +0530

    final update for day-2

--- codes/day-2/react-first-app/src/components/Welcome.js
+++ codes/day-2/react-first-app/src/components/Welcome.js
@@ -7,19 +7,37 @@ import React from 'react'
  *   showFn:[Function]
  * }
  */
-export function Welcome(attributes) {
+export function Welcome(props) {
+    //console.log(this)
     //attributes.dataValue++; <--don't mutate property values received from parent component
     //they are all readonly
     const messageToParent = 'message from child';
+    const callback = (e) => {
+        props.showFn(e, messageToParent)
+    }
     return (
         <div>
             <br />
-            { /*<button onClick={attributes.showFn}>Show from Welcome</button>*/}
-            <button onClick={(e) => attributes.showFn(e, messageToParent)}>Show from Welcome</button>
+            { /*<button onClick={props.showFn}>Show from Welcome</button>*/}
+            {/* <button onClick={callback}>Show from Welcome</button> */}
+            <button onClick={(e) => props.showFn(e, messageToParent)}>Show from Welcome</button>
             <br />
-            <span>Message:&nbsp;{attributes.displayMessage}</span>
+            <span>Message:&nbsp;{props.displayMessage}</span>
             <br />
-            <span>Data:&nbsp;{attributes.dataValue}</span>
+            <span>Data:&nbsp;{props.dataValue}</span>
         </div>
     );
 }
+/**
+ * {
+ *  type:'div',
+ *  ref:null,
+ *  key:null,
+ *  props:{
+ *   displayMessage:'Welcome to React JS',
+ *   dataValue:10,
+ *   showFn:[Function],
+ *   children:[<br/>,<butoon/>,br,span,br,span]
+ *  }
+ * }
+ */
--- codes/day-2/react-first-app/src/container/App.js
+++ codes/day-2/react-first-app/src/container/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Welcome } from '../components/Welcome';
 import './App.css';
 
+const Check = (prop) => {
+  return <span>{prop.val}</span>;
+}
 function App() {
   const message = 'Welcome to React JS';
   const data = 10;
@@ -10,10 +13,14 @@ function App() {
     args.forEach((item) => console.log(item))
     console.log(e.target)
   }
+  const welcomeElement = <Welcome displayMessage={message} datadataValueue={data} showFn={show} />
+  console.log(welcomeElement)
   return (
     <div className="App">
       <button onClick={(e) => { show(e, 10, 'joy') }}>Show from App</button>
-      <Welcome displayMessage={message} dataValue={data} showFn={show} />
+      {/* <Welcome displayMessage={message} dataValue={data} showFn={show} /> */}
+      <br />
+      {welcomeElement}
     </div>
   );
 }
