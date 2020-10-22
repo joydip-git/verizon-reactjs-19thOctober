e56e7ee875ea8805591c2110edbb655452e15de0
siemens-c2c <joy_mon78@yahoo.co.in>
2020-10-22 17:45:41 +0530

    notes updated

---
+++ codes/day-4/product-management-system/src/components/Common/DashBoard/DashBoard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function DashBoard() {
+    return (
+        <nav className='navbar navbar-default'>
+            <a href='#'>
+                Home
+            </a>
+            <a href='#'>
+                Products
+            </a>
+            <a href='#'>
+                Add
+            </a>
+        </nav>
+    )
+}
--- codes/day-4/product-management-system/src/containers/App/App.js
+++ codes/day-4/product-management-system/src/containers/App/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import ProductContainer from '../Product/ProductContainer'
+import DashBoard from '../../components/Common/DashBoard/DashBoard';
 
 function App() {
   return (
     <div className="App">
+      <DashBoard />
+      <br/>
       <ProductContainer />
     </div>
   );
