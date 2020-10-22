02940b1bd967fe35ede7d5cd660a11983db0ddc6
siemens-c2c <joy_mon78@yahoo.co.in>
2020-10-22 18:12:08 +0530

    route update-1

--- codes/day-4/product-management-system/src/containers/App/App.js
+++ codes/day-4/product-management-system/src/containers/App/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import './App.css';
 import ProductContainer from '../Product/ProductContainer'
+import Home from '../../components/Common/Home/Home'
+import AddProduct from '../../components/Product/AddProduct/AddProduct'
+import UpdateProduct from '../../components/Product/UpdateProduct/UpdateProduct'
+import ProductDetail from '../../components/Product/ProductDetail/ProductDetail'
 import DashBoard from '../../components/Common/DashBoard/DashBoard';
+import { BrowserRouter, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <DashBoard />
-      <br/>
-      <ProductContainer />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <DashBoard />
+        <br />
+        <Route path='/home' component={Home} />
+        <Route path='/products' component={ProductContainer} />
+        <Route path='/product/add' component={AddProduct} />
+        <Route path='/product/:id' component={ProductDetail} />
+        <Route path='/product/update/:id' component={UpdateProduct} />
+      </div>
+    </BrowserRouter>
   );
 }
 
