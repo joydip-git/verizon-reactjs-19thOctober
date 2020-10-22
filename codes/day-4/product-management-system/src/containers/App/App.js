da2f747a01490c2e19e2b7cb1bf730925001a103
siemens-c2c <joy_mon78@yahoo.co.in>
2020-10-22 16:22:19 +0530

    pms update-2

--- codes/day-4/product-management-system/src/containers/App/App.js
+++ codes/day-4/product-management-system/src/containers/App/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import ProductContainer from '../Product/ProductContainer'
 
 function App() {
   return (
     <div className="App">
-      
+      <ProductContainer />
     </div>
   );
 }
---
+++ codes/day-4/product-management-system/src/service/productService.js
@@ -0,0 +1,21 @@
+import { URL } from '../constants/productConstants'
+
+export const getProductRecords = () => {
+
+}
+
+export const getProductRecordById = (productId) => {
+
+}
+
+export const addProductRecord = (newProduct) => {
+
+}
+
+export const updateProductRecord = (updatedProduct) => {
+
+}
+
+export const deleteProductRecordById = (productId) => {
+
+}
