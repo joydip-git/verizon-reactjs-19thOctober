9c2aa7c2e4198e53db18c8f3cddc8bff99a18563
siemens-c2c <joy_mon78@yahoo.co.in>
2020-10-23 16:52:11 +0530

    pms product detailupdate

--- codes/day-5/product-management-system/src/components/Product/ProductDetail/ProductDetail.js
+++ codes/day-5/product-management-system/src/components/Product/ProductDetail/ProductDetail.js
@@ -1,11 +1,56 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useHistory, useLocation, useParams } from 'react-router-dom';
+import { getProductRecordById } from '../../../service/productService'
 
-export default class ProductDetail extends Component {
-    render() {
-        return (
-            <div>
-                Product detail
-            </div>
-        )
+// export default function ProductDetail({ history, match, location }) {
+//     const pid = parseInt(match.params.id)
+export default function ProductDetail() {
+    const history = useHistory();
+    //useLocation()
+    const allParams = useParams();
+    const pid = parseInt(allParams.id)
+    const [productState, setProductState] = useState({ product: null, errorMessage: '', loading: true });
+
+    useEffect(() => {
+        getProductRecordById(pid)
+            .then(resp => {
+                console.log(resp.data)
+                setProductState({
+                    product: resp.data,
+                    errorMessage: '',
+                    loading: false
+                })
+
+            }).catch(e => {
+                setProductState({
+                    product: null,
+                    errorMessage: e.message,
+                    loading: false
+                })
+            })
+
+        return () => {
+            console.log('dismounted...')
+        }
+    }, []);
+
+    let design = null;
+    if (productState.loading) {
+        design = <span>Loading....</span>
+    } else if (productState.errorMessage !== '') {
+        design = <span>{productState.errorMessage}</span>
+    } else if (productState.product !== null) {
+        design = <div>{productState.product.productName}</div>
+    } else {
+        design = <span>No product found</span>
     }
+    console.log('[PD] rendered')
+    return (
+        <>
+            { design}
+            <br />
+            <button className='btn btn-primary' onClick={() => history.push('/products')}>Back</button>
+        </>
+    )
 }
+
